9732a011b5432a7aea213031c2ebddfb5bd82779
Cheetos2408 <omuralievsanjar29@gmail.com>
2025-12-17 00:01:08 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+const btn = document.getElementById("btn");
+
+const API_KEY = "my api key";
+
+async function getQuote() {
+  quoteEl.textContent = "Загрузка...";
+  authorEl.textContent = "";
+
+  try {
+    const response = await fetch(
+      "https://api.api-ninjas.com/v1/quotes",
+      {
+        headers: {
+          "X-Api-Key": API_KEY
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    quoteEl.textContent = `"${data[0].quote}"`;
+    authorEl.textContent = `— ${data[0].author}`;
+
+  } catch (e) {
+    quoteEl.textContent = "Ошибка загрузки ❌";
+    console.error(e);
+  }
+}
+
+btn.addEventListener("click", getQuote);
+getQuote();
